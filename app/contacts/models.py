4ae162fb57c0a51eead7f6700c52b348083aad14
Ben Rowan <beny3964@gmail.com>
2025-02-12 20:00:29 +0000

    add basic model

--- app/contacts/models.py
+++ app/contacts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=60)
+    address = models.CharField(max_length=240)
+    telephone_number = PhoneNumberField(region="GB")
+
+    def __str__(self):
+        return self.name
