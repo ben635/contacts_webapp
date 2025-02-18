ae4b317baf8859b697dc8bdae0559983309c9aea
Ben Rowan <beny3964@gmail.com>
2025-02-18 21:22:32 +0000

    add styling to list of all contacts

--- app/contacts/urls.py
+++ app/contacts/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path("<int:contact_id>/", views.list_contact, name="Contact"),
+    path("<int:contact_id>/delete/", views.delete_contact, name="Delete Contact"),
     path("contacts/", views.contacts, name="All Contacts"),
     path("create_contact", views.create_contacts, name="create_contact")
 ]
--- app/contacts/views.py
+++ app/contacts/views.py
@@ -10,11 +10,20 @@ from django.http import HttpResponseRedirect
 
 from .models import Contact
 from .serializers import ContactSerializer
+from django.http import JsonResponse
 
 def list_contact(request, contact_id):
     response = "The name of this contact is %s"
     try:
         return HttpResponse(response % Contact.objects.get(pk=contact_id))
+    except Contact.DoesNotExist:
+        raise Http404("Contact does not exist.")
+    
+def delete_contact(request, contact_id):
+    try:
+        contact = Contact.objects.get(pk=contact_id)
+        contact.delete()
+        return HttpResponse("Success!")
     except Contact.DoesNotExist:
         raise Http404("Contact does not exist.")
 
@@ -26,11 +35,23 @@ def contacts(request):
     return render(request, "contacts/index.html", context)
 
 def create_contacts(request):
-    newContact = Contact(name=request.POST["name"], address=request.POST["address"], telephone_number=request.POST["telephone_number"])
-
+    newContact = Contact(
+        name=request.POST["name"],
+        address=request.POST["address"],
+        telephone_number=request.POST["telephone_number"]
+    )
     newContact.save()
 
-    return HttpResponseRedirect(reverse("Contact", args=(newContact.id,)))
+    data = {
+        "success": True,
+        "contact": {
+            "id": newContact.id,
+            "name": newContact.name,
+            "address": newContact.address,
+            "telephone_number": str(newContact.telephone_number),
+        }
+    }
+    return JsonResponse(data)
 
 class ContactViewSet(viewsets.ModelViewSet):
     """
